1c4d83ce88462e1944bb7b087905e071a596e125
rakesh635 <rakesh635@gmail.com>
2019-01-05 15:07:06 +0530

    BUILD SUCCESS
    BUILD SUCCESS

--- src/main/java/com/schoology/app/Runner.java
+++ src/main/java/com/schoology/app/Runner.java
@@ -3,7 +3,7 @@ package com.schoology.app;
 import com.schoology.app.output.Output;
 
 /**
- * Created by SleepWalker on 06.06.2016.
+ *  Created by SleepWalker on 06.06.2016.
  */
 public class Runner {
     public static void main(String[] args) {
